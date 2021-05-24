04c27a22d32098a900b9fa3fec27252d03f56fab
josuegerardo96 <josuegerardo09@gmail.com>
2021-05-24 14:31:00 -0600

    Personajes en prolog

---
+++ personajes.pl
@@ -0,0 +1,16 @@
+character(aristocrate).
+character(bard).
+character(clown).
+character(cultist).
+character(spirit).
+character(healer).
+character(king).
+character(mask).
+character(poprigunchik).
+character(prince).
+character(princess).
+character(snow).
+character(mohawk).
+character(child).
+character(oldman).
+character(doll).
