8e851b9c0a8f7a5c79b79fc5018a87b79f8cf6b1
josuegerardo96 <josuegerardo09@gmail.com>
2021-05-24 14:33:16 -0600

    Personajes en database

--- dataBase.pl
+++ dataBase.pl
@@ -13,4 +13,19 @@ weapon(bow).
 weapon(potion).
 
 
-character().
+character(aristocrate).
+character(bard).
+character(clown).
+character(cultist).
+character(spirit).
+character(healer).
+character(king).
+character(mask).
+character(poprigunchik).
+character(prince).
+character(princess).
+character(snow).
+character(mohawk).
+character(child).
+character(oldman).
+character(doll).
