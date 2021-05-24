10d4cacfc4e1410bab2680ee6be4e3101830c009
brydiaz <bryandiaz13301@gmail.com>
2021-05-24 16:36:58 -0600

    Principio de logica

--- dataBase.pl
+++ dataBase.pl
@@ -14,88 +14,103 @@ weapon(potion).
 
 
 hasWeapon(Name, Weapon):-
-    character(Name,weapon(Weapon),,);
-    character(Name,,weapon(Weapon),);
-    character(Name,,,weapon(Weapon)).
+    character(Name,weapon(Weapon),_,_);
+    character(Name,_,weapon(Weapon),_);
+    character(Name,_,_,weapon(Weapon)).
 
+:- dynamic(character/5).
+:- dynamic(askWepon/1).
 %S P W
-character(aristocrate,
+character(1,aristocrate,
     weapon(potion),
     weapon(wand),
     weapon(shuriken)).
 %B C S
-character(bard,
+character(2,bard,
     weapon(bow),
     weapon(crossbow),
     weapon(spear)).
 %C G P
-character(clown,
+character(3,clown,
     weapon(potion),
     weapon(gem),
     weapon(shuriken)).
 %D M W
-character(cultist,
+character(4,cultist,
     weapon(wand),
     weapon(dreamCatcher),
     weapon(mallet)).
 %C K S 
-character(spirit,
+character(5,spirit,
     weapon(spear),
     weapon(crossbow),
     weapon(key)).
 %A M S 
-character(healer,
+character(6,healer,
     weapon(sword),
     weapon(mullet),
     weapon(axe)).
 %B K S
-character(king,
+character(7,king,
     weapon(sword),
     weapon(bow),
     weapon(key)).
 %G S S
-character(mask,
+character(8,mask,
     weapon(shuriken),
     weapon(sword),
     weapon(gem)).
 
 %F S W
-character(poprigunchik,
+character(9,poprigunchik,
     weapon(wand),
     weapon(spear),
     weapon(flail)).
 %A C F
-character(prince,
+character(10,prince,
     weapon(flail),
     weapon(axe),
     weapon(crossbow)).
 %B D S
-character(princess,
+character(11,princess,
     weapon(bow),
     weapon(dreamCatcher),
     weapon(spear)).
 %P S K 
-character(snow,
+character(12,snow,
     weapon(potion),
     weapon(shuriken),
     weapon(key)).
 %A F M
-character(mohawk,
+character(13,mohawk,
     weapon(axe),
     weapon(flail),
     weapon(mallet)).
 %B  G S
-character(child,
+character(14,child,
     weapon(bow),
     weapon(sword),
     weapon(gem)).
 %D P W
-character(oldman,
+character(15,oldman,
     weapon(wand),
     weapon(dreamCatcher),
     weapon(potion)).
 %F G M
-character(doll,
+character(16,doll,
     weapon(mallet),
     weapon(flail),
     weapon(gem)).
+
+generateGuilty():-
+    random(1,17,RandomID),
+    character(RandomID,Name, W1,W2, W3),
+    assert(guilty(RandomID,Name, W1,W2, W3)).
+    
+askWepon(Wepon):-
+    guilty(_, _, weapon(Wepon),_,_); retract(character(_,_, weapon(Wepon),_,_)),
+    guilty(_, _, _,weapon(Wepon),_); retract(character(_,_, _,weapon(Wepon),_)),
+    guilty(_, _, _,_,weapon(Wepon)); retract(character(_,_, _,_,weapon(Wepon))).
+     
+    
+    
