a8094589a20b9e7790a6bb308ae0ce78ba4ae93d
brydiaz <bryandiaz13301@gmail.com>
2021-05-24 14:29:58 -0600

    RG v2

---
+++ dataBase.pl
@@ -0,0 +1,16 @@
+weapon(sword).
+weapon(shuriken).
+weapon(spear).
+weapon(shuriken).
+weapon(mallet).
+weapon(axe).
+weapon(flail).
+weapon(axe).
+weapon(crossbow).
+weapon(bow).
+weapon(wand).
+weapon(bow).
+weapon(potion).
+
+
+character().
