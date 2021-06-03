5dbffc2cedd89512d2f397342f70da654b367368
brydiaz <bryandiaz13301@gmail.com>
2021-06-02 20:12:14 -0600

    Listo las pistas

---
+++ RoyalTraitorFinal/src/royaltraitor/Game.java
@@ -0,0 +1,65 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package royaltraitor;
+
+import java.util.ArrayList;
+import logic.Prolog;
+import logic.Weapon;
+import logic.Character;
+import view.MainView;
+import org.jpl7.Query;
+import org.jpl7.Term;
+import view.MainView;
+
+/**
+ *
+ * @author bryan
+ */
+public class Game {
+    public boolean gameOn = true;
+    public boolean winner = false;
+    public Prolog dataBase = new Prolog();
+    public ArrayList<Weapon> weapons = dataBase.getWeapons(); //SE OBTIENEN LAS ARMAS
+    public ArrayList<logic.Character> characters1 = dataBase.getCharacters(); //SE OBTIENEN LAS ARMAS
+    public ArrayList<logic.Character> characters ;
+    public logic.Character guilty = dataBase.getGuilty();//SE OBTIENE EL CULPABLE
+    public ArrayList<logic.Character> suspects;
+    public int x = dataBase.make2IntialSuspects();
+
+    public Game() {
+        //SE GENERAN DOS POSIBLES SOSPECHOZOS
+        this.suspects = dataBase.getSuspects(); //SE OBTIENEN LOS SOSPECHOSOS
+        //suspects = dataBase.askWeapon("wand");//PREGUNTARA A LA BASES SI EL CULPABLE TIENE ESA ARMA, SI NO LA TIENE REVISAR SI UN SOSPECHOSLO LA TIENE Y LO ELIMINA
+        this.characters = dataBase.actCharacters(this.characters1, this.suspects);
+    }
+    
+    public void gameOn() throws InterruptedException{
+        
+        ArrayList road = new ArrayList();
+        for(int i=0;i<15;i++){
+            road.add(0);
+        }
+        int posX = 0;
+        road.set(posX,1);
+        posX += 1;
+    
+        
+        MainView screen = new MainView(this,road);
+        screen.resizeBackground();
+
+
+       
+        screen.initialDraws(characters, road);
+        System.out.println(guilty.getName());
+        //while(!winner && (int)road.get(road.size()-1)!=1){
+            //System.out.println("");
+            
+        //}
+        System.out.println("Ganaste");
+       
+    }
+    
+}
---
+++ RoyalTraitorFinal/src/royaltraitor/RoyalTraitor.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package royaltraitor;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Map;
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+import logic.Prolog;
+import logic.Weapon;
+import logic.Character;
+import org.jpl7.Query;
+import org.jpl7.Term;
+import view.MainView;
+
+/**
+ *
+ * @author bryan
+ */
+public class RoyalTraitor {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws InterruptedException {
+        // TODO code application logic here
+        Game game = new Game();
+        game.gameOn();
+        
+    }
+    
+}
